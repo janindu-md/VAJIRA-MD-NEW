e2a8c8d907d5d5a97cfd68761ad5d3ee3fca8c23
janindu-md <peshalajanindu0@gmail.com>
2024-08-16 09:16:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=zVUWGbpA#OB8lZO376-Wb2TeeE_n_vFFK8-4_QDw8_4BDHTtzzUU' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=DEdAxLJI#F7jkZOvz9tb6nl1En_vKyN9x_f21T7r5qbnOOobGwjI' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
